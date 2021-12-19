77077924e10343b14f5e41ba87a0ae508372fc8f
Chady <chady.ayoub@hotmail.com>
2021-12-19 18:07:35 +0200

    Added Register Screen

--- app/src/main/java/com/prohacker/busapp/activities/Login.java
+++ app/src/main/java/com/prohacker/busapp/activities/Login.java
@@ -2,18 +2,15 @@ package com.prohacker.busapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.prohacker.busapp.R;
 
-public class SplashScreen extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        Intent intent = new Intent(SplashScreen.this, Login.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_login);
     }
 }
